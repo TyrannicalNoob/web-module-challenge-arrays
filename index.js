c1276091a2c963bb9ee3997606d0f4bd2569c185
Michael Cavellier <tyrannicalnoobz@gmail.com>
2021-01-20 13:40:45 -0500

    solved prob

--- index.js
+++ index.js
@@ -160,8 +160,8 @@ Use the filterByWord function below to do the following:
     DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-    /*your code here*/
+function filterByWord(arr, string){
+    for(let i = 0);
 }
 
 
